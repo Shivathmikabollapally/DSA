2f14b24fbc85307e9c9b4065536ba3689c2ec90f
Shivathmikabollapally <shivathmikabollapally@gmail.com>
2025-11-07 19:30:52 +0530

    Add files via upload

---
+++ Easy/Twosum.java
@@ -0,0 +1,21 @@
+public class Twosum {
+    // Brute-force O(n^2) solution (no HashMap)
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] { i, j };
+                }
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    // Quick test
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] res = new Twosum().twoSum(nums, target);
+        System.out.println(res[0] + ", " + res[1]);
+    }
+}
